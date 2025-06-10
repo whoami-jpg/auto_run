d8661f7b456a0b195774e26845f6b91c1b9a232f
kerrm <kerrm@qq.com>
2025-06-11 01:04:31 +0800

    <modify>:[send request,but source code of web is diff to real web code]

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # auto_run
 python: auto run
+
+进度：
+  成功发送request获取网页源码
+  但是经过检查源码与网页源码不一致
+  需要修改request中的结构模拟下请求，看是否能拿到满意的结果
--- request.py
+++ request.py
@@ -1,16 +1,34 @@
 import requests
 import os
-#为了模拟正常用户的访问，我们可以修改request的请求头参数：
-    #包含主机域名，User-Agent(正常访问时会带有浏览器类型和版本，以及电脑操作系统等),Accept等
-head = {}
-#设定要get info的网址
-url = "https://www.youpin898.com/"
-url_info = "url.html"
-#创建存储url信息的文件
-result = os.system("mkdir %s" %(url_info))
-#获取网页信息
-response = requests.get(url)
-if response.ok:
-    print("请求成功")
-else:
-    print("请求失败")
+from bs4 import BeautifulSoup
+#为了模拟正常用户的访问，我们可以修改request的请求头参数：包含主机域名，User-Agent(正常访问时会带有浏览器类型和版本，以及电脑操作系统等),Accept等
+
+def GetSourceInfo():
+    #设定要get info的网址
+    url = {
+        'cs2':"https://steamdt.com/cs2/%E2%98%85%20M9%20Bayonet%20|%20Gamma%20Doppler%20(Factory%20New)"
+    }
+    #获取网页信息
+    response = requests.get(url['cs2'])
+    response.encoding = 'utf-8'
+    with open('target_web.html','w') as f:
+        f.write(response.text)
+    #解析html
+    soup = BeautifulSoup(response.text,"html.parser")
+    soup_text = soup.prettify()
+    with open('soup_text.html','w') as f1:
+        f1.write(soup_text)
+    #从页面中找到某些东西
+    # findAll：树状查找
+    # find：查找某个
+    typelist = soup.find_all("div",attrs={"class":"relative mr-10 w-486"})#.find("div",attrs={"class":"text-color-green"})
+    typelist_text = str(typelist)
+    with open('typelist.html','w') as f2:
+        f2.write(typelist_text)
+
+def main():
+    GetSourceInfo()
+
+
+if __name__ == '__main__':
+    main()
